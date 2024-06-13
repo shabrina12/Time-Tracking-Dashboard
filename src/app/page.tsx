3145118825b26fd6b2cde9f565f4f54488d72033
Shabrina Putri <shabbrina5@gmail.com>
2024-06-13 20:34:49 +0700

    edit position card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import iconWork from '../../public/images/icon-work.svg'
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen items-center justify-center">
-      <div className="grid gap-12 xs:grid-cols-1 md:grid-cols-3">
-        <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
-        <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
-        <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
-        <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
+    <main className="relative bg-Very-dark-blue min-h-screen flex items-center justify-center">
+      <div className="grid xs:grid-cols-1 md:grid-cols-3 xs:gap-20 md:gap-12">
+          <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
+          <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
+          <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
+          <Card icon={iconWork} bgColor="bg-Light-red" title="Work" hours="32hrs" subtitle="Last Week - 36hrs" />
+   
       </div>
     </main>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,16 +12,16 @@ type CardProps = {
 
 const Card = ({title, hours, subtitle, bgColor, icon} : CardProps) => {
     return (
-        <div className={`${bgColor} relative container text-white h-full w-[400px] rounded-3xl`}>
-            <div className='h-1/5 rounded-t-lg flex justify-end pr-4'>
-                <Image src={icon} alt='icon work' />
+        <div className={`${bgColor} relative xs:mb-40 md:mb-0 container text-white h-full w-[400px] rounded-t-3xl`}>
+            <div className='rounded-t-lg flex justify-end pr-4'>
+                <Image src={icon} alt='icon work' className="h-[70px]" />
             </div>
             <div className='absolute top-12 bg-Dark-blue w-full px-10 py-12 rounded-3xl'>
                 <div className='flex justify-between items-center mb-8'>
                     <h1 className='text-xl'>{title}</h1>
                     <Image src={ellipsis} alt='icon' className="w-[35px] h-[10px]" />
                 </div>
-                <div className='flex xs:flex-row xs:items-center xs:justify-between md:items-start md:flex-col gap-4'>
+                <div className='flex xs:flex-row xs:justify-between xs:items-center md:items-start md:flex-col gap-4'>
                     <h1 className='text-5xl'>{hours}</h1>
                     <p className='text-Pale-Blue'>{subtitle}</p>
                 </div>
